8f4671afe46e28822b32bc4d19b88d2257de9311
SeungpilPark <sppark@uengine.org>
2017-07-25 19:11:09 +0900

    url fix

--- static/js/super-search.js
+++ static/js/super-search.js
@@ -4,6 +4,7 @@
 */
 
 (function () {
+    var siteUrl = document.querySelector('#site-url').value;
     var isSearchOpen = false,
         searchEl = document.querySelector('#js-search'),
         searchInputEl = document.querySelector('#js-search__input'),
@@ -63,7 +64,7 @@
     }
 
     var xmlhttp=new XMLHttpRequest();
-    xmlhttp.open("GET","/sitemap.xml");
+    xmlhttp.open("GET", siteUrl + "/sitemap.xml");
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState != 4) return;
         if (xmlhttp.status != 200 && xmlhttp.status != 304) { return; }
